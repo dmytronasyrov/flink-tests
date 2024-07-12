dc007562a0f65fc18d198887036675d63bdf9191
Dmytro Nasyrov <dmytro@pharosproduction.com>
2024-07-13 00:16:52 +0300

    update

--- src/main/java/app/WindowFunctions.java
+++ src/main/java/app/WindowFunctions.java
@@ -5,12 +5,18 @@ import app.game.PlayerRegistered;
 import app.game.ServerEvent;
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.shaded.guava31.com.google.common.collect.Iterators;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.AllWindowedStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
@@ -67,10 +73,74 @@ public class WindowFunctions {
           }
         }
 
-        collector.collect(String.format("Window [%d - %d] %d", timeWindow.getStart(), timeWindow.getEnd(), registeredEvents));
+        collector.collect(String.format("Window A [%d - %d] %d", timeWindow.getStart(), timeWindow.getEnd(), registeredEvents));
       }
     });
     apply.print();
+
+    final SingleOutputStreamOperator<String> process = serverEventTimeWindowAllWindowedStream.process(new ProcessAllWindowFunction<ServerEvent, String, TimeWindow>() {
+      @Override
+      public void process(final ProcessAllWindowFunction<ServerEvent, String, TimeWindow>.Context context, final Iterable<ServerEvent> input, final Collector<String> collector) throws Exception {
+        int registeredEvents = 0;
+        final TimeWindow window = context.window();
+
+        for (ServerEvent event : input) {
+          if (event instanceof PlayerRegistered) {
+            registeredEvents++;
+          }
+        }
+
+        collector.collect(String.format("Window B [%d - %d] %d", window.getStart(), window.getEnd(), registeredEvents));
+      }
+    });
+    process.print();
+
+    final SingleOutputStreamOperator<Long> aggregate = serverEventTimeWindowAllWindowedStream.aggregate(new AggregateFunction<ServerEvent, Long, Long>() {
+      @Override
+      public Long createAccumulator() {
+        return 0L;
+      }
+
+      @Override
+      public Long add(final ServerEvent serverEvent, final Long acc) {
+        if (serverEvent instanceof PlayerRegistered)
+          return acc + 1L;
+        else
+          return acc;
+      }
+
+      @Override
+      public Long getResult(final Long acc) {
+        return acc;
+      }
+
+      @Override
+      public Long merge(final Long acc2, final Long acc1) {
+        return acc1 + acc2;
+      }
+    });
+    aggregate.print();
+
+    final KeyedStream<ServerEvent, String> serverEventStringKeyedStream = serverEventSingleOutputStreamOperator.keyBy(event -> event.getClass().getSimpleName());
+    final SingleOutputStreamOperator<String> apply1 = serverEventStringKeyedStream.window(tumblingEventTimeWindows)
+      .apply(new WindowFunction<ServerEvent, String, String, TimeWindow>() {
+        @Override
+        public void apply(final String key, final TimeWindow timeWindow, final Iterable<ServerEvent> input, final Collector<String> collector) throws Exception {
+          final String output = String.format("AAAA Key: %s, Window: %s, %d", key, timeWindow, Iterators.size(input.iterator()));
+          collector.collect(output);
+        }
+      });
+    apply1.print();
+
+    final SingleOutputStreamOperator<String> process1 = serverEventStringKeyedStream.window(tumblingEventTimeWindows)
+      .process(new ProcessWindowFunction<ServerEvent, String, String, TimeWindow>() {
+        @Override
+        public void process(final String key, final ProcessWindowFunction<ServerEvent, String, String, TimeWindow>.Context context, final Iterable<ServerEvent> input, final Collector<String> collector) throws Exception {
+          final String output = String.format("BBBB Key: %s, Window: %s, %d", key, context.window(), Iterators.size(input.iterator()));
+          collector.collect(output);
+        }
+      });
+    process1.print();
 
     env.execute();
   }
